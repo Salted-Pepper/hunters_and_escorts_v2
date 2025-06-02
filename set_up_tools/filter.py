d464ff3b982e746fe4af9997cd534331a20d1125
salted-pepper <robkieneker@gmail.com>
2025-06-03 09:18:39 +1000

    Created subdirectory for not directly used scripts

--- set_up_tools/filter.py
+++ set_up_tools/filter.py
@@ -1,14 +1,14 @@
 import json
 
 # Read the current coalition_ships_all.json file
-with open('data/coalition_aircraft.json', 'r') as file:
+with open('../data/coalition_aircraft.json', 'r') as file:
     ships_data = json.load(file)
 
 # Filter ships that have "Submarine Detection Skill" not equal to null
 filtered_ships = [ship for ship in ships_data if ship.get("Submarine Detection Skill") is not None]
 
 # Write the filtered data to coalition_ships_onlyASW.json
-with open('data/coalition_aircraft_onlyASW.json', 'w') as file:
+with open('../data/coalition_aircraft_onlyASW.json', 'w') as file:
     json.dump(filtered_ships, file, indent=2)
 
 print(f"Filtered from {len(ships_data)} ships to {len(filtered_ships)} ships with Submarine Detection Skill")
